7e4f9383b75588b10712aa334afdb78ee4a4c724
Luis Lopez <luis.lopezespinosa@colorado.edu>
2014-12-04 13:10:28 -0600

    added variables to core properties and updated osdd ontology

--- lib/btriple.py
+++ lib/btriple.py
@@ -1,54 +1,100 @@
 #!/usr/bin/env python
 
 import rdflib
-from pprint import pprint
+import re
 import xml.etree.ElementTree as ET
-from dns.tsig import validate
 
 
-class OSDD():
+class Parser():
     '''
-    This class transforms OSDD documents represented in XML
-    into a n-triple file so it can be used by a triple store.
+    This class contains helper methods to parse XML documents.
     '''
-    def find_os_element(self, element, tag):
+
+    def __init__(self, url):
+        tree = ET.parse(url)
+        document = tree.getroot()
+        self.doc = document
+
+    def set_ns(self, ns):
+        self.NS = ns
+
+    def find_node(self, nanmespaces, element, tag):
         '''
-        Finds elements from the root document that use the
-        OpenSearch namespace. Namespaces can vary so an array
-        of their possible forms is used in the matching function.
+        Finds elements from the root documents. Namespaces can vary therefore
+        an array of their possible forms is used in the matching function.
         '''
-        os_schemas = ['{http://a9.com/-/spec/opensearch/1.1/}']
-        for namespace in os_schemas:
-            found = element.findall(namespace+tag)
+        for ns in nanmespaces:
+            found = element.findall(ns+tag)
             if found is not None:
                 return found
             else:
                 continue
         return None
 
+    def find(self, tag):
+        '''
+        Finds elements based on the default namespace set and the document root
+        '''
+        for ns in self.NS:
+            found = self.doc.findall(ns+tag)
+            if found is not None:
+                return found
+            else:
+                continue
+        return None
+
+    def get_namespaces(self):
+        uri_set = set()
+        for elem in self.doc.getiterator():
+            if elem.tag[0] == "{":
+                uri, tag = elem.tag[1:].split("}")
+                uri_set.add(uri)
+        return uri_set
+
+class OSDD():
+    '''
+    This class transforms OSDD documents represented in XML
+    into a n-triple file so it can be used by a triple store.
+    '''
+
     def validate_opensearch(self):
         '''
         Simple naive validation, has an A9 namespace and
-        and endpoint.
+        and has at least one endpoint.
         '''
-        if self.doc is None:
-            return
-        endpoint = self.find_os_element(self.doc, 'Url')[0].attrib['template']
-        if 'http://a9.com' in self.doc.tag and endpoint is not None:
+        if self.parser is None:
+            return None
+        endpoint = self.parser.find('Url')[0].attrib['template']
+        if 'http://a9.com' in self.parser.doc.tag and endpoint is not None:
             self.is_valid = True
         else:
             self.is_valid = False
             self.doc = None
+    def extract_url_template_variables(self):
+        variables = set()
+        for endpoint in self.endpoints:
+            match = re.findall("{(.*?)}", endpoint)
+            variables.update(match)
+        return variables
+
 
     def extract_core_properties(self):
         '''
         Extracts the main properties from a OSDD to populate
         an RDFlib graph.
         '''
-        self.endpoint = self.find_os_element(self.doc, 'Url')[0].attrib['template']
-        self.description = self.find_os_element(self.doc, 'Description')[0]
+        self.profile = "OpenSearch"
+        self.endpoints = [item.attrib['template'] for item in
+                          self.parser.find('Url')]
+        self.description = self.parser.find('Description')[0].text
+        self.namespaces = self.parser.get_namespaces()
+        self.variables = self.extract_url_template_variables()
 
-    def __init__(self, document):
-        self.doc = document
+
+
+    def __init__(self, parser):
+        ns = ['{http://a9.com/-/spec/opensearch/1.1/}']
+        self.parser = parser
+        self.parser.set_ns(ns)
         self.validate_opensearch()
         self.extract_core_properties()
--- test/test_btriple.py
+++ test/test_btriple.py
@@ -1,18 +1,26 @@
 import unittest
-from btriple import OSDD
-import xml.etree.ElementTree as ET
+from btriple import Parser, OSDD
 
 
 class TestOSDD(unittest.TestCase):
     def setUp(self):
-        tree = ET.parse('test/relevant-documents/opensearch-nasa.xml')
-        document = tree.getroot()
-        self.osdd = OSDD(document)
+        parser = Parser('test/relevant-documents/opensearch-nasa.xml')
+        self.osdd = OSDD(parser)
 
     def test_btriple_validates_osdd(self):
         self.assertIsInstance(self.osdd, OSDD)
         self.assertTrue(self.osdd.is_valid is True)
 
     def test_btriple_extracts_core_properties(self):
-        osdd_endpoint = 'http://modwebsrv.modaps.eosdis.nasa.gov/axis2/services/MODAPSservices/getOpenSearch?products={MODAPSParameters:products}&collection={MODAPSParameters:collection?}&start={time:start}&stop={time:stop}&bbox={geo:box}&coordsOrTiles={MODAPSParameters:coordsOrTiles?}&dayNightBoth={MODAPSParameters:dayNightBoth?}'
-        self.assertEqual(self.osdd.endpoint, osdd_endpoint)
+        osdd_endpoints = ['http://modwebsrv.modaps.eosdis.nasa.gov/axis2/services/MODAPSservices/getOpenSearch?products={MODAPSParameters:products}&collection={MODAPSParameters:collection?}&start={time:start}&stop={time:stop}&bbox={geo:box}&coordsOrTiles={MODAPSParameters:coordsOrTiles?}&dayNightBoth={MODAPSParameters:dayNightBoth?}',
+        'http://modwebsrv.modaps.eosdis.nasa.gov/axis2/services/MODAPSservices/getOpenSearch?products={MODAPSParameters:products}&collection={MODAPSParameters:collection?}&start={time:start}&stop={time:stop}&bbox={geo:box}&coordsOrTiles={MODAPSParameters:coordsOrTiles?}']
+        osdd_description = 'Use MODAPS Web Services to search for various MODIS related data products'
+        osdd_namespaces = set()
+        osdd_namespaces.add('http://a9.com/-/spec/opensearch/1.1/')
+        osdd_variables = set()
+        osdd_variables.update(['geo:box', 'time:start', 'MODAPSParameters:dayNightBoth?', 'MODAPSParameters:coordsOrTiles?', 'MODAPSParameters:products', 'time:stop', 'MODAPSParameters:collection?'])
+
+        self.assertEqual(self.osdd.endpoints, osdd_endpoints)
+        self.assertEqual(self.osdd.description, osdd_description)
+        self.assertEqual(self.osdd.namespaces, osdd_namespaces)
+        self.assertEqual(self.osdd.variables, osdd_variables)
